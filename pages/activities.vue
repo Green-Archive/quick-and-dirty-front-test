5b29afb42ed464d0a2b4a5d07a093e9448926532
Seki Darubi <6431503002@lamduan.mfu.ac.th>
2023-07-26 19:54:47 +0700

    first commit

---
+++ pages/activities.vue
@@ -0,0 +1,239 @@
+<template>
+  <v-container fluid class="pa-0">
+    <v-toolbar elevation="4">
+      <v-toolbar-title>Activities</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-dialog v-model="dialog" max-width="500px">
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn color="primary" dark class="mb-2" v-bind="attrs" v-on="on">
+            <v-icon left> mdi-plus </v-icon>
+
+            Add
+          </v-btn>
+        </template>
+        <v-card>
+          <v-card-title class="blue">
+            <span class="text-h5">{{ formTitle }}</span>
+          </v-card-title>
+
+          <v-card-text>
+            <v-container>
+              <v-row>
+                <v-col cols="12">
+                  <v-text-field
+                    v-model="editedItem.name"
+                    label="Activity name"
+                  ></v-text-field>
+                </v-col>
+              </v-row>
+            </v-container>
+          </v-card-text>
+
+          <v-card-actions class="d-flex justify-center">
+            <v-btn color="blue darken-1" outlined @click="close">Cancel</v-btn>
+            <v-btn color="blue darken-1" @click="save">Confirm</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+
+      <v-dialog v-model="dialogDelete" max-width="500px">
+        <v-card>
+          <v-card-title class="red text-h5">Delete</v-card-title>
+
+          <v-card-text>
+            <v-container>
+              <v-row justify="center">
+                <v-col cols="12" sm="6" md="4" class="d-flex justify-center">
+                  <div class="text-h5">Confirm</div>
+                </v-col>
+              </v-row>
+            </v-container>
+          </v-card-text>
+
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="blue darken-1" outlined @click="closeDelete"
+              >Cancel</v-btn
+            >
+            <v-btn color="red darken-1" @click="deleteItemConfirm"
+              >Confirm</v-btn
+            >
+            <v-spacer></v-spacer>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+    </v-toolbar>
+
+    <v-row>
+      <v-col cols="12">
+        <v-data-table
+          :headers="headers"
+          :items="activities"
+          class="elevation-1"
+        >
+          <template v-slot:item.actions="{ item }">
+            <v-icon small class="mr-2" @click="editItem(item)">
+              mdi-pencil
+            </v-icon>
+            <v-icon small @click="deleteItem(item)"> mdi-delete </v-icon>
+          </template>
+          <template v-slot:no-data>
+            <!-- <v-btn color="primary" @click="initialize"> Reset </v-btn> -->
+            <v-progress-linear
+              indeterminate
+              rounded
+              height="6"
+            ></v-progress-linear>
+          </template>
+        </v-data-table>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    dialog: false,
+    dialogDelete: false,
+    headers: [
+      {
+        text: "Activities",
+        align: "start",
+        sortable: false,
+        value: "name",
+      },
+
+      { text: "Actions", value: "actions", sortable: false },
+    ],
+    activities: [],
+    editedIndex: -1,
+    editedItem: {
+      name: "",
+    },
+    defaultItem: {
+      name: "",
+    },
+  }),
+
+  computed: {
+    formTitle() {
+      return this.editedIndex === -1 ? "Create" : "Edit";
+    },
+  },
+
+  created() {
+    this.fetch_activities();
+    // this.initialize();
+  },
+
+  methods: {
+    initialize() {
+      this.activities = [
+        {
+          name: "Activity 1",
+        },
+      ];
+    },
+
+    editItem(item) {
+      this.editedIndex = this.activities.indexOf(item);
+      this.editedItem = Object.assign({}, item);
+      this.dialog = true;
+    },
+
+    deleteItem(item) {
+      this.editedIndex = this.activities.indexOf(item);
+      this.editedItem = Object.assign({}, item);
+      this.dialogDelete = true;
+    },
+
+    deleteItemConfirm() {
+      //   this.activities.splice(this.editedIndex, 1);
+      this.deleteActivity(this.editedItem._id);
+      this.closeDelete();
+    },
+
+    close() {
+      this.dialog = false;
+      this.$nextTick(() => {
+        this.editedItem = Object.assign({}, this.defaultItem);
+        this.editedIndex = -1;
+      });
+    },
+
+    closeDelete() {
+      this.dialogDelete = false;
+      this.$nextTick(() => {
+        this.editedItem = Object.assign({}, this.defaultItem);
+        this.editedIndex = -1;
+      });
+    },
+
+    save() {
+      if (this.editedIndex > -1) {
+        this.updateActivity();
+        // Object.assign(this.activities[this.editedIndex], this.editedItem);
+      } else {
+        // this.activities.push(this.editedItem);
+        this.handleSubmitForm();
+      }
+      //   this.fetch_activities();
+      this.close();
+    },
+
+    async fetch_activities() {
+      // const apiURL = "https://hayai-hllc-test-hzri43hw3q-as.a.run.app";
+      const apiURL = "/activity";
+
+      try {
+        const res = await this.$axios.$get(apiURL);
+        this.activities = res;
+        console.log(this.activities);
+        console.log("Done");
+      } catch (error) {
+        console.log(error);
+      }
+    },
+
+    handleSubmitForm() {
+      const apiURL = "/activity/create-activity";
+
+      this.$axios
+        .$post(apiURL, this.editedItem)
+        .then((res) => {
+          this.fetch_activities();
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+
+    async updateActivity() {
+      const apiURL = `/activity/update-activity/${this.editedItem._id}`;
+
+      try {
+        await this.$axios.$put(apiURL, { name: this.editedItem.name });
+        console.log("Activity updated successfully");
+        this.fetch_activities();
+      } catch (error) {
+        console.log(error);
+      }
+    },
+
+    async deleteActivity(id) {
+      const apiURL = `/activity/delete-activity/${id}`;
+
+      try {
+        await this.$axios.$delete(apiURL);
+        console.log("Activity deleted successfully");
+        this.fetch_activities();
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
+};
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,59 @@
+<template>
+  <v-container>
+    <v-row class="mb-12" justify="center" align="center">
+      <div class="text-h1">Login</div>
+    </v-row>
+
+    <v-row justify="center" align="center">
+      <v-text-field
+      v-model="user.username"
+        outlined
+        label="User"
+        prepend-inner-icon="mdi-account"
+      ></v-text-field>
+    </v-row>
+
+    <v-row justify="center" align="center">
+      <v-text-field
+      v-model="user.password"
+        outlined
+        label="Password"
+        prepend-inner-icon="mdi-lock"
+        type="password"
+      ></v-text-field>
+    </v-row>
+    <v-row justify="center" align="center">
+      
+      <v-btn @click="singIn"> Login </v-btn>
+      <!-- <v-btn to="/activities"> Login </v-btn> -->
+
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "Login",
+  data: () => ({
+    user: {
+      username: "",
+      password: "",
+    },
+  }),
+  created() {},
+  methods: {
+    async singIn() {
+      const apiURL = "/auth/signin";
+
+      try {
+        const res = await this.$axios.$post(apiURL, this.user);
+        this.$router.push("/");
+        console.log(res);
+        console.log("Done");
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
+};
+</script>
