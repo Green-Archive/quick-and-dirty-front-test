7d06fff314d7ff534237481b0d12a25429f523a1
Seki Darubi <6431503002@lamduan.mfu.ac.th>
2023-09-07 15:51:19 +0700

    v2

--- nuxt.config.js
+++ nuxt.config.js
@@ -53,7 +53,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: "http://127.0.0.1:5000/api/v1/",
+    baseURL: "http://puangun.mfu.ac.th/api/v2/",
     // baseURL: "https://mental-back.mfu-cits.com/api/v1/",
     // credentials: true,
   },
